1c39d49174db55ab4e742047170aec2d215e8dd5
JAJOBA <wuheimin@gmail.com>
2014-04-16 11:00:34 +0800

    Github contribution test

--- app/src/main/java/g0v/ly/lylog/legislator/Profile.java
+++ app/src/main/java/g0v/ly/lylog/legislator/Profile.java
@@ -16,9 +16,6 @@ public class Profile extends Fragment implements RestApiCallback {
 
 	TextView tvResponse;
 
-	public Profile() {
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
