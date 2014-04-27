f2eac18750b9a3dc15b36256e14092aaa28834f2
JAJOBA <wuheimin@gmail.com>
2014-04-27 17:00:49 +0800

    Add custom adapter.

--- app/src/main/java/g0v/ly/lylog/NavigationDrawerFragment.java
+++ app/src/main/java/g0v/ly/lylog/NavigationDrawerFragment.java
@@ -83,7 +83,7 @@ public class NavigationDrawerFragment extends Fragment {
 				getActionBar().getThemedContext(),
 				android.R.layout.simple_list_item_activated_1,
 				android.R.id.text1,
-				new String[]{
+				new String[] {
 						getString(R.string.title_section1),
 						getString(R.string.title_section2),
 						getString(R.string.title_section3),
