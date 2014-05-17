87b72690eca75a5059c74db2db185ff50f2ec1ba
JAJOBA <wuheimin@gmail.com>
2014-05-17 15:39:47 +0800

    Create package and fragment.

---
+++ app/src/main/java/g0v/ly/lylog/bill/ProfileBill.java
@@ -0,0 +1,12 @@
+package g0v.ly.lylog.bill;
+
+import android.support.v4.app.Fragment;
+
+import g0v.ly.lylog.rest.RestApiCallback;
+
+public class ProfileBill extends Fragment implements RestApiCallback {
+	@Override
+	public void getDone(String response, long spendTime, int page) {
+
+	}
+}
