6a65e70cc9e55e181e8aab8fef20af4d4efab207
JAJOBA <wuheimin@gmail.com>
2014-05-17 15:33:22 +0800

    Remove unuse comments.

--- app/src/main/java/g0v/ly/lylog/rest/RESTFunctionManager.java
+++ app/src/main/java/g0v/ly/lylog/rest/RESTFunctionManager.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 public class RESTFunctionManager {
 
 	public void restGet(String getUrl, RestApiCallback restApiCallback) {
-		//Log.i("RESTFunctionManager :: restGet", "(0) in restGet");
 		RESTGetAsyncTask restGetAsyncTask  = new RESTGetAsyncTask(getUrl, restApiCallback);
 		restGetAsyncTask.execute();
 	}
@@ -34,27 +33,24 @@ public class RESTFunctionManager {
 
 			DefaultHttpClient 	client 	= new DefaultHttpClient();
 			HttpGet 			request = new HttpGet(getUrl);
-			//Log.i("RESTFunctionManager :: ThreadRESTGet", "(1) getUrl: " + getUrl);
 			try {
 				HttpResponse response 	= client.execute(request);
 				responseStr 			= EntityUtils.toString(response.getEntity(), "UTF-8");
-				//Log.i("RESTFunctionManager :: ThreadRESTGet", "(2) get responseStr");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			spendTime = System.currentTimeMillis() - spendTime;
-			//Log.i("RESTFunctionManager :: ThreadRESTGet", "(3) spend " + spendTime / 1000 +
-			//		"." + spendTime % 1000 + " sec to get response.");
 			return null;
 		}
 
 		@Override
 		protected void onPostExecute(Void aVoid) {
 			super.onPostExecute(aVoid);
+			int page;
 			String[] temp;
+
 			temp = getUrl.split("=");
-			int page = Integer.valueOf(temp[1].substring(0, temp[1].lastIndexOf("&")));
-			//Log.d("RESTFunctionManager :: ThreadRESTGet", "page: " + page);
+			page = Integer.valueOf(temp[1].substring(0, temp[1].lastIndexOf("&")));
 			restApiCallback.getDone(responseStr, spendTime, page);
 		}
 	}
